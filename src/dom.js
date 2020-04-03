bb3dab84ef8765b993f075fd3b27b03f3b0840d5
Wilfried Canirinka <canirinkawilfried@gmail.com>
2020-04-03 22:33:47 +0200

    Applied changes from review

--- src/dom.js
+++ src/dom.js
@@ -2,65 +2,60 @@
 /* eslint-disable max-len */
 import setPositionForWeatherInfo from './positionWeather';
 
+const errorMessage = document.getElementById('errorMessage');
 const init = (resultFromServer) => {
-  switch (resultFromServer.weather[0].main) {
-    case 'Clear':
-      document.body.style.backgroundImage = 'url("./../dist/images/clear.jpeg")';
-      break;
+  if (typeof resultFromServer !== 'string') {
+    switch (resultFromServer.weather[0].main) {
+      case 'Clear':
+        document.body.style.backgroundImage = 'url("./../dist/images/clear.jpeg")';
+        break;
 
-    case 'Clouds':
-      document.body.style.backgroundImage = 'url("./../dist/images/cloudy.jpeg")';
-      break;
+      case 'Clouds':
+        document.body.style.backgroundImage = 'url("./../dist/images/cloudy.jpeg")';
+        break;
 
-    case 'Rain':
-    case 'Drizzle':
-    case 'Mist':
-      document.body.style.backgroundImage = 'url("./../dist/images/rain.jpeg")';
-      break;
+      case 'Rain':
+      case 'Drizzle':
+      case 'Mist':
+        document.body.style.backgroundImage = 'url("./../dist/images/rain.jpeg")';
+        break;
 
-    case 'Thunderstorm':
-      document.body.style.backgroundImage = 'url("./../dist/images/storm.jpeg")';
-      break;
+      case 'Thunderstorm':
+        document.body.style.backgroundImage = 'url("./../dist/images/storm.jpeg")';
+        break;
 
-    case 'Snow':
-      document.body.style.backgroundImage = 'url("./../dist/images/snow.jpeg")';
-      break;
+      case 'Snow':
+        document.body.style.backgroundImage = 'url("./../dist/images/snow.jpeg")';
+        break;
 
-    default:
-      break;
-  }
-
-  const celsiusToFahrenheit = temperature => (temperature * 9) / 5 + 32;
-
-  const weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
-  const temperatureElement = document.getElementById('temperature');
-  const humidityElement = document.getElementById('humidity');
-  const windSpeedElement = document.getElementById('windSpeed');
-  const cityHeader = document.getElementById('cityHeader');
-  const weatherIcon = document.getElementById('documentIconImg');
-
-  weatherIcon.src = `http://openweathermap.org/img/wn/${resultFromServer.weather[0].icon}.png`;
-
-  const resultDescription = resultFromServer.weather[0].description;
-  weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
-
-  temperatureElement.innerHTML = `${Math.floor(resultFromServer.main.temp)}&#176` + ' C';
-  temperatureElement.addEventListener('click', () => {
-    if (
-      temperatureElement.innerHTML === `${Math.floor(resultFromServer.main.temp)}°` + ' C'
-    ) {
-      let fahrenheit = celsiusToFahrenheit(resultFromServer.main.temp);
-      fahrenheit = Math.floor(fahrenheit);
-      temperatureElement.innerHTML = `${fahrenheit}&#176 F`;
-    } else {
-      temperatureElement.innerHTML = `${Math.floor(resultFromServer.main.temp)}&#176` + ' C';
+      default:
+        break;
     }
-  });
-  windSpeedElement.innerHTML = `Winds at ${Math.floor(resultFromServer.wind.speed)} m/s`;
-  cityHeader.innerHTML = resultFromServer.name;
-  humidityElement.innerHTML = `Humidity levels at ${resultFromServer.main.humidity}%`;
 
-  setPositionForWeatherInfo();
+    const weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
+    const temperatureElement = document.getElementById('temperature');
+    const humidityElement = document.getElementById('humidity');
+    const windSpeedElement = document.getElementById('windSpeed');
+    const cityHeader = document.getElementById('cityHeader');
+    const weatherIcon = document.getElementById('documentIconImg');
+
+    weatherIcon.src = `http://openweathermap.org/img/wn/${resultFromServer.weather[0].icon}.png`;
+
+    const resultDescription = resultFromServer.weather[0].description;
+    weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
+
+    temperatureElement.innerHTML = `${Math.floor(resultFromServer.main.temp)}&#176`;
+    windSpeedElement.innerHTML = `Winds at ${Math.floor(resultFromServer.wind.speed)} m/s`;
+    cityHeader.innerHTML = resultFromServer.name;
+    humidityElement.innerHTML = `Humidity levels at ${resultFromServer.main.humidity}%`;
+
+    setPositionForWeatherInfo();
+  } else {
+    errorMessage.innerText = resultFromServer;
+    if (!errorMessage.hasAttribute('style')) {
+      errorMessage.setAttribute('style', 'display: block !important');
+    }
+  }
 };
 
 export default init;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,63 @@
 /* eslint-disable radix */
+import '@babel/polyfill';
 import init from './dom';
 
+const errorMessage = document.getElementById('errorMessage');
 const appId = '70485a8e13b31fcaee6010aadf0328be';
-const units = 'metric';
 let searchMethod;
 
-const getSearchMethod = searchTerm => {
+const getSearchMethod = (searchTerm) => {
   if (searchTerm.length === 5 && `${Number.parseInt(searchTerm)}` === searchTerm) {
     searchMethod = 'zip';
   } else searchMethod = 'q';
 };
 
-const searchWeather = searchTerm => {
-  getSearchMethod(searchTerm);
-  fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result => result.json()).then(result => { init(result); });
+const searchWeather = async (searchTerm, units) => {
+  try {
+    if (errorMessage.hasAttribute('style')) {
+      errorMessage.removeAttribute('style');
+    }
+    getSearchMethod(searchTerm);
+    const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`,
+      {
+        mode: 'cors',
+      });
+
+    if (result.status === 200) {
+      return result.json();
+    }
+    return 'City not found';
+  } catch (error) {
+    return error.message;
+  }
 };
 
-document.getElementById('searchBtn').addEventListener('click', () => {
-  const searchTerm = document.getElementById('searchInput').value;
-  if (searchTerm) searchWeather(searchTerm);
+document.getElementById('celsiusBtn').addEventListener('click', async () => {
+  try {
+    const searchTerm = document.getElementById('searchInput').value;
+    if (searchTerm) {
+      const response = await searchWeather(searchTerm, 'metric');
+      init(response);
+    }
+  } catch (error) {
+    errorMessage.innerText = error.message;
+    if (!errorMessage.hasAttribute('style')) {
+      errorMessage.setAttribute('style', 'display: block !important');
+    }
+  }
+});
+
+document.getElementById('fahrenheitBtn').addEventListener('click', async () => {
+  try {
+    const searchTerm = document.getElementById('searchInput').value;
+    if (searchTerm) {
+      const response = await searchWeather(searchTerm, 'imperial');
+      init(response);
+    }
+  } catch (error) {
+    errorMessage.innerText = error.message;
+    if (!errorMessage.hasAttribute('style')) {
+      errorMessage.setAttribute('style', 'display: block !important');
+    }
+  }
 });
